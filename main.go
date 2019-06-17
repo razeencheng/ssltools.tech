f94168c5d81a1ecc1be91e8a87ddb32416b0b489
Razeen Cheng <razeen.cheng@gmail.com>
2019-06-17 17:25:49 +0800

    feat: 创建演示页面

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/kataras/iris"
+)
+
+func main() {
+	app := iris.Default()
+
+	tmpl := iris.HTML("./public", ".html")
+	tmpl.Reload(os.Getenv("RUN_MODE") == "dev")
+
+	app.StaticWeb("/public", "./public")
+	app.RegisterView(tmpl)
+	app.Get("/", HandleHome)
+	app.Get("demo/{id}", HandleDemoView)
+
+	app.Run(iris.Addr(":8080"))
+}
+
+// Site doc
+type Site struct {
+	Title     string
+	Author    string
+	Demos     *DemoTable
+	CopyRight string
+	Beian     string
+}
+
+// DemoTable doc
+type DemoTable struct {
+	THead  []string
+	Fields []*DemoField
+}
+
+// DemoField doc
+type DemoField struct {
+	ID     int
+	Fields []string
+	URL    string
+}
+
+var siteInfo = Site{
+	Title:  "SSL Demo 演示网站",
+	Author: "By Razeen",
+	Demos: &DemoTable{
+		THead: []string{"名称", "描述"},
+		Fields: []*DemoField{
+			&DemoField{1, []string{"TLSv1.3 常规演示页面", "TLSv1.3 开启基本特性"}, "https://tls13.ssltools.tech"},
+			&DemoField{2, []string{"TLSv1.3 常规演示页面", "TLSv1.3 开启基本特性"}, "https://tls13.ssltools.tech"},
+		},
+	},
+	CopyRight: "2019 SSLTOOLS.TECH",
+	Beian:     "沪ICP备19009734号-1",
+}
+
+// Demo doc
+type Demo struct {
+	Title string
+	Desc  string
+}
+
+// HandleHome 首页
+func HandleHome(c iris.Context) {
+	c.ViewData("", siteInfo)
+	c.View("index.html")
+}
+
+// HandleDemoView 处理不同Demo页展示
+func HandleDemoView(c iris.Context) {
+
+	demo := &Demo{
+		Title: "Demo 不存在",
+	}
+
+	id, err := strconv.Atoi(c.Params().Get("id"))
+	if err == nil {
+		for _, v := range siteInfo.Demos.Fields {
+			if v.ID == id {
+				demo.Title = v.Fields[0]
+				demo.Desc = v.Fields[1]
+				return
+			}
+		}
+	}
+
+	c.ViewData("", demo)
+	c.View("demo.html")
+	return
+}
